f85a372547a663de559a7db96bd43562e9979340
Castor Köhler <castor.kohler@futurice.com>
2019-08-09 12:56:19 +0300

    3.1

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-53235323",
+    "id": 1
+  },
+  {
+    "name": "Martti Tienari",
+    "number": "123",
+    "id": 2
+  },
+  {
+    "name": "Arto Järvinen",
+    "number": "040-123456",
+    "id": 3
+  },
+  {
+    "name": "Lea Kutvonen",
+    "number": "040-123456",
+    "id": 4
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
