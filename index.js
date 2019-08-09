c1e76a9e30af285ac3a4458c97187cb4034b41d4
Castor Köhler <castor.kohler@futurice.com>
2019-08-09 13:01:27 +0300

    3.2

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const amountStr = `Phonebook has info for ${persons.length} people`
+  const dateStr = new Date()
+  res.send(`<p>${amountStr}</p><p>${dateStr}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
